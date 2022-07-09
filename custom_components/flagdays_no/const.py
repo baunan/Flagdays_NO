e8897dd29a9169a5223fe8cec7c374eb0c12caac
baunan <59838425+baunan@users.noreply.github.com>
2022-07-09 16:29:24 +0200

    Update const.py

--- custom_components/flagdays_no/const.py
+++ custom_components/flagdays_no/const.py
@@ -4,15 +4,16 @@ CONF_FLAGS_DAYS = "flagdays"
 CONF_HIDE_PAST = "hide_past"
 CONF_TIME_OFFSET = "time_offset"
 CONF_PLATFORM = "sensor"
-DEFAULT_COORDINATES = {"lat": 55.395903819648304, "lon": 10.388097722778282}
-DEFAULT_FLAG = "dannebrog"
+DEFAULT_COORDINATES = {"lat": 59.2181, "lon": 10.9298}
+DEFAULT_FLAG = "norge"
 DEFAULT_TIME_OFFSET = 10
-DOMAIN = "flagdays_dk"
-FLAGDAY_URL = "https://www.justitsministeriet.dk/temaer/flagning/flagdage/"
+DOMAIN = "flagdays_no"
+FLAGDAY_URL = "http://www.flaggdager.no/"
 UPDATE_INTERVAL = 60
 
 CREDITS = [
     {"Created by": "J-Lindvig (https://github.com/J-Lindvig)"},
-    {"Data provided by": "Justitsministeriet (" + FLAGDAY_URL + ")"},
+    {"Redeveloped by": "B. Aunan"},
+    {"Data provided by": "Flaggdager (" + FLAGDAY_URL + ")"},
     {"Sunrise/sunset provided by": "Sunrise-Sunset (https://sunrise-sunset.org/api)"},
 ]
